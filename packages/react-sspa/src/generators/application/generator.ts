1444d40fb6cc82646c7f72bec63dbd3f193c8467
Conan-Ding <cding@ameripharma.us>
2023-12-29 16:06:31 -0800

    w.i.p: temporary save

--- packages/react-sspa/src/generators/application/generator.ts
+++ packages/react-sspa/src/generators/application/generator.ts
@@ -1,32 +1,84 @@
 import {
-    addProjectConfiguration,
-    formatFiles,
-    generateFiles,
-    GeneratorCallback,
-    joinPathFragments,
-    names,
-    Tree,
-    runTasksInSerial,
-  } from '@nx/devkit';
-  import { initGenerator, initGenerator as jsInitGenerator } from '@nx/js';
-import {ReactSingleSpaNxGeneratorSchema, NormalizedSchema} from './schema';
-import {normalizeOptions} from './lib/normalize-options';
-import {getReactSingleSPAProjectTargets} from './lib/get-react-single-spa-project-targets';
+  ProjectConfiguration,
+  addProjectConfiguration,
+  formatFiles,
+  generateFiles,
+  GeneratorCallback,
+  joinPathFragments,
+  names,
+  Tree,
+  runTasksInSerial,
+  addDependenciesToPackageJson,
+  removeDependenciesFromPackageJson,
+} from '@nx/devkit';
+import { reactDomVersion, reactVersion } from '@nx/react';
+import { initGenerator, initGenerator as jsInitGenerator } from '@nx/js';
+import { ReactSingleSpaNxGeneratorSchema, NormalizedSchema , InitSchema} from './schema';
+import { normalizeOptions } from './lib/normalize-options';
+import { getReactSingleSPAProjectTargets } from './lib/get-react-single-spa-project-targets';
+
+function addProject(tree: Tree, options: NormalizedSchema) {
+  const project: ProjectConfiguration = {
+    root: options.projectRoot,
+    sourceRoot: joinPathFragments(options.projectRoot, 'src'),
+    projectType: 'application',
+    targets: getReactSingleSPAProjectTargets(options),
+  };
+
+  addProjectConfiguration(tree, options.projectRoot, {
+    ...project,
+  });
+}
 
 function addFiles(tree: Tree, options: NormalizedSchema) {
-    const templateOptions = {
-        ...options,
-        ...names(options.singleSPAOrg),
-        ...names(options.projectName),
-        templ: '',   
-    }
-    generateFiles(
-        tree,
-        joinPathFragments(__dirname, 'files'),
-        options.projectRoot,
-        templateOptions
-    )
+  const templateOptions = {
+    ...options,
+    ...names(options.singleSPAOrg),
+    ...names(options.projectName),
+    templ: '',
+  };
+  generateFiles(
+    tree,
+    joinPathFragments(__dirname, 'files'),
+    options.projectRoot,
+    templateOptions
+  );
 }
+//TODO fix this,
+function updateDependencies(host: Tree, schema: InitSchema) {
+    removeDependenciesFromPackageJson(host, ['@nx/react'], []);
+
+    // how do i determine the version of react, react-dom, etc to install?
+    // in original nx, there is a constance file, but I want to just use default react version from nx
+    const dependencies = {
+      react: reactVersion,
+      'react-dom': reactDomVersion,
+    };
+  
+    if (!schema.skipHelperLibs) {
+      dependencies['tslib'] = tsLibVersion;
+    }
+  
+    return addDependenciesToPackageJson(host, dependencies, {
+      '@nx/react': nxVersion,
+      '@types/node': typesNodeVersion,
+      '@types/react': typesReactVersion,
+      '@types/react-dom': typesReactDomVersion,
+      '@testing-library/react': testingLibraryReactVersion,
+    });
+}
+  
+// 
+// async function addWebpack(tree: Tree, options: NormalizedSchema) {
+//     const { webpackInitGenerator } = ensurePackage<
+//       typeof import('@nx/webpack')
+//     >('@nx/webpack', nxVersion);
+//     const webpackInitTask = await webpackInitGenerator(host, {
+//       uiFramework: 'react',
+//       skipFormat: true,
+//     });
+//     tasks.push(webpackInitTask);
+// }
 /** 
 * Generate app with this plugin
 * @summary 
@@ -37,28 +89,30 @@ function addFiles(tree: Tree, options: NormalizedSchema) {
 * @param {ParamDataTypeHere} parameterNameHere - Brief description of the parameter here. Note: For other notations of data types, please refer to JSDocs: DataTypes command.
 * @return {ReturnValueDataTypeHere} Brief description of the returning value here.
 */
-export async function appGenerator(tree: Tree, schema: ReactSingleSpaNxGeneratorSchema):Promise<GeneratorCallback>  {
-    const options:NormalizedSchema = await normalizeOptions(tree, schema);
-    const tasks: GeneratorCallback[] = [];
-    await jsInitGenerator(tree, {
-        skipFormat: true,
-      });
-    addProjectConfiguration(tree, options.projectName, {
-        name: options.projectName,
-        root: options.projectRoot,
-        projectType: 'application',
-        sourceRoot: joinPathFragments(options.projectRoot, 'src'),
-        tags: [],
-        targets: getReactSingleSPAProjectTargets(options),    
-    });
-    addFiles(tree, options);
+// TODO update root package.json, add dependencies and scripts
+export async function appGenerator(
+  tree: Tree,
+  schema: ReactSingleSpaNxGeneratorSchema
+): Promise<GeneratorCallback> {
+  const options: NormalizedSchema = await normalizeOptions(tree, schema);
+  const tasks: GeneratorCallback[] = [];
+  await jsInitGenerator(tree, {
+    skipFormat: true,
+  });
+  addProject(tree, options);
+  addFiles(tree, options);
 
-    tasks.push(await initGenerator(tree, {
-        skipFormat: true,
-    }));
+  tasks.push(
+    await initGenerator(tree, {
+      skipFormat: true,
+    })
+  );
+  if (!schema.skipPackageJson) {
+    const installTask = updateDependencies(tree, schema);
+    tasks.push(installTask);
+  }
 
-    return runTasksInSerial(...tasks);
+  return runTasksInSerial(...tasks);
 }
 
-
-export default appGenerator
+export default appGenerator;
--- packages/react-sspa/src/generators/application/schema.d.ts
+++ packages/react-sspa/src/generators/application/schema.d.ts
@@ -1,12 +1,20 @@
 import { Linter } from '@nx/eslint';
 import type { ProjectNameAndRootFormat } from '@nx/devkit/src/generators/project-name-and-root-utils';
 
+export interface InitSchema {
+    skipFormat?: boolean;
+    skipPackageJson?: boolean;
+    skipHelperLibs?: boolean;
+    js?: boolean;
+    rootProject?: boolean;
+  }
 
 export interface ReactSingleSpaNxGeneratorSchema {
     organization: string;
     name: string;
     directory?: string;
     projectNameAndRootFormat?: ProjectNameAndRootFormat;
+    skipPackageJson: boolean;
 }
 
 export interface NormalizedSchema extends ReactSingleSpaNxGeneratorSchema{
